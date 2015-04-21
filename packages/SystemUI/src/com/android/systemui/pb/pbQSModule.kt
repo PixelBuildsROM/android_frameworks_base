5f8f99be510ffa5940fa01669da5e98a5c2f90fb
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    SystemUI: Add heads up tile
    xyyx: adapted for P and replaced drawbles
    
    Change-Id: Ied99d31630cd85bbb85f176987fbcf38d197d0cf
    Signed-off-by: xyyx <xyyx@mail.ru>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- packages/SystemUI/src/com/android/systemui/pb/pbQSModule.kt
+++ packages/SystemUI/src/com/android/systemui/pb/pbQSModule.kt
@@ -18,6 +18,7 @@ package com.android.systemui.pb
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.CaffeineTile
+import com.android.systemui.qs.tiles.HeadsUpTile
 
 import dagger.Binds
 import dagger.Module
@@ -32,5 +33,11 @@ interface pbQSModule {
     @IntoMap
     @StringKey(CaffeineTile.TILE_SPEC)
     fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
+
+    /** Inject HeadsUpTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(HeadsUpTile.TILE_SPEC)
+    fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
 
 }
