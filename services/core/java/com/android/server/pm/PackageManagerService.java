b75d2c5168d594098816fd0f7a5a63edac3522d4
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-09-03 10:23:31 -0300

    PackageManagerService: Protect forced enabled components at runtime
    Change-Id: I1dc15786d08cde1879df8b7c6f9b73c4443ff168
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -908,6 +908,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
     private final PackageProperty mPackageProperty = new PackageProperty();
 
     ArrayList<ComponentName> mDisabledComponentsList;
+    ArrayList<ComponentName> mForceEnabledComponentsList;
 
     final PendingPackageBroadcasts mPendingBroadcasts;
 
@@ -2247,8 +2248,10 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                     com.android.internal.R.array.config_globallyDisabledComponents), false);
 
             // Enable components marked for forced-enable at build-time
+            mForceEnabledComponentsList = new ArrayList<ComponentName>();
             enableComponents(mContext.getResources().getStringArray(
                     com.android.internal.R.array.config_forceEnabledComponents), true);
+            loadForceEnabledComponents();
 
             // If this is first boot after an OTA, then we need to clear code cache directories.
             // Note that we do *not* clear the application profiles. These remain valid
@@ -2433,6 +2436,15 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         mServiceStartWithDelay = SystemClock.uptimeMillis() + (60 * 1000L);
 
         Slog.i(TAG, "Fix for b/169414761 is applied");
+    }
+
+    private void loadForceEnabledComponents(){
+        String[] components = mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_forceEnabledComponents);
+        for (String name : components) {
+            ComponentName cn = ComponentName.unflattenFromString(name);
+            mForceEnabledComponentsList.add(cn);
+        }
     }
 
     private void enableComponents(String[] components, boolean enable) {
@@ -5905,6 +5917,13 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                 return;
             }
 
+            // Don't allow to control components forced enabled at build-time
+            if (mForceEnabledComponentsList.contains(componentName)) {
+                Slog.d(TAG, "Ignoring attempt to control forced enabled component "
+                        + componentName.flattenToString());
+                return;
+            }
+            
             setEnabledSettings(List.of(new PackageManager.ComponentEnabledSetting(componentName, newState, flags)),
                     userId, callingPackage);
         }
