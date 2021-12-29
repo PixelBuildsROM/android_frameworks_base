8b9948f967f7025fc09b715726cfdeb16b051c3f
PixelBoot <bootingpixels@gmail.com>
2021-12-29 01:11:22 -0800

    Revert "base: do not use new lockscreen layout for bypass"
    This reverts commit d7a2141f20f4564cfcb081b7fa488a0adfc03d30.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBypassController.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBypassController.kt
@@ -63,8 +63,6 @@ open class KeyguardBypassController : Dumpable {
         get() = field && mKeyguardStateController.isFaceAuthEnabled
         private set
 
-    var bypassEnabledBiometric: Boolean = false
-
     var faceUnlockMethod: Int = 0
 
     var bouncerShowing: Boolean = false
@@ -104,24 +102,13 @@ open class KeyguardBypassController : Dumpable {
             }
         })
 
-        if (context.resources.getBoolean(
-                com.android.internal.R.bool.config_faceAuthOnlyOnSecurityView)){
-            bypassEnabledBiometric = false
-        }else{
-            tunerService.addTunable(object : TunerService.Tunable {
-                override fun onTuningChanged(key: String?, newValue: String?) {
-                    faceUnlockMethod = tunerService.getValue(key, 0)
-                }
-            }, Settings.Secure.FACE_UNLOCK_METHOD)
-            val dismissByDefault = if (context.resources.getBoolean(
-                            com.android.internal.R.bool.config_faceAuthDismissesKeyguard)) 1 else 0
-            tunerService.addTunable(object : TunerService.Tunable {
-                override fun onTuningChanged(key: String?, newValue: String?) {
-                    bypassEnabledBiometric = (faceUnlockMethod == 0 &&
-                        tunerService.getValue(key, dismissByDefault) != 0)
-                }
-            }, Settings.Secure.FACE_UNLOCK_DISMISSES_KEYGUARD)
-        }
+        val dismissByDefault = if (context.resources.getBoolean(
+                        com.android.internal.R.bool.config_faceAuthDismissesKeyguard)) 1 else 0
+        tunerService.addTunable(object : TunerService.Tunable {
+            override fun onTuningChanged(key: String?, newValue: String?) {
+                bypassEnabled = tunerService.getValue(key, dismissByDefault) != 0
+            }
+        }, Settings.Secure.FACE_UNLOCK_DISMISSES_KEYGUARD)
         lockscreenUserManager.addUserChangedListener(
                 object : NotificationLockscreenUserManager.UserChangedListener {
                     override fun onUserChanged(userId: Int) {
@@ -139,8 +126,8 @@ open class KeyguardBypassController : Dumpable {
         biometricSourceType: BiometricSourceType,
         isStrongBiometric: Boolean
     ): Boolean {
-        if (bypassEnabledBiometric) {
-            val can = biometricSourceType != BiometricSourceType.FACE || canBypass()
+        if (bypassEnabled) {
+            val can = canBypass()
             if (!can && (isPulseExpanding || qSExpanded)) {
                 pendingUnlock = PendingUnlock(biometricSourceType, isStrongBiometric)
             }
@@ -164,7 +151,7 @@ open class KeyguardBypassController : Dumpable {
      * If keyguard can be dismissed because of bypass.
      */
     fun canBypass(): Boolean {
-        if (bypassEnabledBiometric) {
+        if (bypassEnabled) {
             return when {
                 bouncerShowing -> true
                 statusBarStateController.state != StatusBarState.KEYGUARD -> false
@@ -180,7 +167,7 @@ open class KeyguardBypassController : Dumpable {
      * If shorter animations should be played when unlocking.
      */
     fun canPlaySubtleWindowAnimations(): Boolean {
-        if (bypassEnabledBiometric) {
+        if (bypassEnabled) {
             return when {
                 statusBarStateController.state != StatusBarState.KEYGUARD -> false
                 qSExpanded -> false
@@ -203,7 +190,6 @@ open class KeyguardBypassController : Dumpable {
             pw.println("  mPendingUnlock: $pendingUnlock")
         }
         pw.println("  bypassEnabled: $bypassEnabled")
-        pw.println("  bypassEnabledBiometric: $bypassEnabledBiometric")
         pw.println("  canBypass: ${canBypass()}")
         pw.println("  bouncerShowing: $bouncerShowing")
         pw.println("  isPulseExpanding: $isPulseExpanding")
