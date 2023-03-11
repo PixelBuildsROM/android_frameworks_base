1023dd3e130eaca0148fb9129f52df22df525315
Henrik Baard <henrik.baard@sony.com>
2023-03-11 12:48:28 +0100

    UI improvement of network activity indicator on status bar
    The activity indicators which can be turned on by a configuration,
    shouldShowActivityConfig will jup around in current implementation.
    
    This change will instead use opacity to indicate activity, using
    grey when no activity and white when active. This will lead to
    the icons keeping the same position all the time.
    
    Bug: 266797961
    Test: adb shell settings put global sysui_demo_allowed 1
          adb shell am broadcast -a com.android.systemui.demo
            -e command enter
          adb shell am broadcast -a com.android.systemui.demo
            -e command network -e fully true -e wifi show
            -e ssid demoSsid -e level 3 -e activity in
          adb shell am broadcast -a com.android.systemui.demo
            -e command network -e fully true -e mobile show
            -e level 3 -e activity in
    Flag: ACONFIG com.android.systemui.status_bar_static_inout_indicators DEVELOPMENT
    
    Co-Author: Leif Martensson <leif.martensson@sony.com>
    Change-Id: Ifc971566799ba22c0f98f86b4fbf3ff47360a3b1

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/viewmodel/MobileIconViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/viewmodel/MobileIconViewModel.kt
@@ -17,6 +17,7 @@
 package com.android.systemui.statusbar.pipeline.mobile.ui.viewmodel
 
 import com.android.settingslib.AccessibilityContentDescriptions.PHONE_SIGNAL_STRENGTH
+import com.android.systemui.Flags.statusBarStaticInoutIndicators
 import com.android.systemui.common.shared.model.ContentDescription
 import com.android.systemui.common.shared.model.Icon
 import com.android.systemui.log.table.logDiffsForTable
@@ -180,7 +181,10 @@ constructor(
             .stateIn(scope, SharingStarted.WhileSubscribed(), false)
 
     override val activityContainerVisible: Flow<Boolean> =
-        activity
-            .map { it != null && (it.hasActivityIn || it.hasActivityOut) }
+        if (statusBarStaticInoutIndicators()) {
+                flowOf(constants.shouldShowActivityConfig)
+            } else {
+                activity.map { it != null && (it.hasActivityIn || it.hasActivityOut) }
+            }
             .stateIn(scope, SharingStarted.WhileSubscribed(), false)
 }
