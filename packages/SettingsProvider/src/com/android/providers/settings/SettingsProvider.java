095544bd9539eaf7a4af0963bc9b62f69f5cdb7d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-07-14 07:56:18 +0800

    core: Workaround for ASI crash
    test: reboot with patch applied, check if ASI services crashes
    
    Change-Id: I104727c30ab34a69cc76955d084ef6741dafc13b
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -1162,6 +1162,12 @@ public class SettingsProvider extends ContentProvider {
         enforceWritePermission(Manifest.permission.WRITE_DEVICE_CONFIG);
         final String callingPackage = resolveCallingPackage();
 
+        try {
+            if (!callingPackage.equals("com.google.android.gms")) {
+                enforceWritePermission(Manifest.permission.WRITE_DEVICE_CONFIG);
+            }
+        } catch (SecurityException e) {}
+
         synchronized (mLock) {
             if (getSyncDisabledModeConfigLocked() != SYNC_DISABLED_MODE_NONE) {
                 return SET_ALL_RESULT_DISABLED;
