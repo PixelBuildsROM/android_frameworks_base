5a4a9fed878d0b93aace253ca5c4e37097c4c7f7
Sumedh Sen <sumedhsen@google.com>
2023-11-21 23:40:11 +0000

    Revert "Launching updated package after finishing package installer"
    This reverts commit 791697e5e7db36d557d6559754798cba0309f280.
    
    Reason for revert: The original change along with ag/24499340 caused opening newly installed apps from Pia to fail.
    
    ag/24499340 is required fix b/202942446 and thus, we cannot revert it. Due to this, PackageInstallerActivity.java is not changed in this revert, since it is already changed by ag/24499340.
    
    Reverting the original change (ag/23468185) will not open an app after it updated itself. For this, while opening the newly installed app, FLAG_ACTIVITY_CLEAR_TOP is added in InstallSuccess.java. With this flag, if an app instance already exists in the stack, it will come to foreground, also closing Pia along the way (and also Settings - if appOp wasn't granted to the caller. [Only for self update case]).
    
    Bug: 310263615
    Bug: 282618106
    Test: Manual. Perform repro steps in b/310263615#comment1
    Test: Manual. Perform repro steps in b/282618106#comment1
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:6f6037224566c80ad79bd193e3e2215b3df79349)
    
    Change-Id: Ia5c5b462a9302ed7f7675d173a4399ff6b37d036

--- packages/PackageInstaller/src/com/android/packageinstaller/InstallSuccess.java
+++ packages/PackageInstaller/src/com/android/packageinstaller/InstallSuccess.java
@@ -17,6 +17,7 @@
 package com.android.packageinstaller;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -110,7 +111,12 @@ public class InstallSuccess extends AlertActivity {
         Button launchButton = mAlert.getButton(DialogInterface.BUTTON_POSITIVE);
         if (enabled) {
             launchButton.setOnClickListener(view -> {
-                setResult(Activity.RESULT_OK, mLaunchIntent);
+                try {
+                    startActivity(mLaunchIntent.addFlags(
+                        Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP));
+                } catch (ActivityNotFoundException | SecurityException e) {
+                    Log.e(LOG_TAG, "Could not start activity", e);
+                }
                 finish();
             });
         } else {
