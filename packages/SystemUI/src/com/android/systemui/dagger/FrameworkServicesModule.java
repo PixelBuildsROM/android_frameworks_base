b410743dd41e14b2675fd0026f8c81a29d338fe7
Tomasz Wasilczyk <twasilczyk@google.com>
2024-03-07 14:57:32 -0800

    Fix crash on potentially missing CarrierConfigManager [SystemUI]
    Bug: 310710841
    Test: it builds and boots
    Flag: NONE
    Change-Id: I379093db2a3170cedaccdeaa8f4eea786304a8a6

--- packages/SystemUI/src/com/android/systemui/dagger/FrameworkServicesModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/FrameworkServicesModule.java
@@ -619,6 +619,7 @@ public class FrameworkServicesModule {
 
     @Provides
     @Singleton
+    @Nullable
     static CarrierConfigManager provideCarrierConfigManager(Context context) {
         return context.getSystemService(CarrierConfigManager.class);
     }
