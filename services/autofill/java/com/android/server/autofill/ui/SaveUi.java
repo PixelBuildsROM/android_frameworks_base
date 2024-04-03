3333ea91186e4b42a05b63886704b17c9dfd3a5b
Tim Yu <yunicorn@google.com>
2024-04-03 18:18:05 +0000

    Fix Autofill Inflating as User 0
    Autofill will inflate views as the current foreground user instead of
    the system_server user (0).
    
    Bug: 327137311
    Test: atest CtsAutoFillServiceTestCases --user-type secondary_user
    Flag: EXEMPT security bug fix
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:2324e00311c0b3326be4389f2905fed270298864
    Merged-In: I8a2c96259976e61d297dec914d10d86e585a6d6f
    Change-Id: I8a2c96259976e61d297dec914d10d86e585a6d6f

--- services/autofill/java/com/android/server/autofill/ui/SaveUi.java
+++ services/autofill/java/com/android/server/autofill/ui/SaveUi.java
@@ -173,6 +173,8 @@ final class SaveUi {
 
     private boolean mDestroyed;
 
+    // System has all permissions, see b/228957088
+    @SuppressWarnings("AndroidFrameworkRequiresPermission")
     SaveUi(@NonNull Context context, @NonNull PendingUi pendingUi,
            @NonNull CharSequence serviceLabel, @NonNull Drawable serviceIcon,
            @Nullable String servicePackageName, @NonNull ComponentName componentName,
@@ -190,7 +192,7 @@ final class SaveUi {
         mComponentName = componentName;
         mCompatMode = compatMode;
 
-        context = new ContextThemeWrapper(context, mThemeId) {
+        context = new ContextThemeWrapper(Helper.getUserContext(context), mThemeId) {
             @Override
             public void startActivity(Intent intent) {
                 if (resolveActivity(intent) == null) {
@@ -229,6 +231,7 @@ final class SaveUi {
                 return null;
             }
         };
+
         final LayoutInflater inflater = LayoutInflater.from(context);
         final View view = inflater.inflate(R.layout.autofill_save, null);
 
