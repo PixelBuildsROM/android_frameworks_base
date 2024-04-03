7601c2bf21b523f02adac8afb945e600007372c7
Tim Yu <yunicorn@google.com>
2024-04-03 18:18:05 +0000

    [RESTRICT AUTOMERGE] Fix Autofill Inflating as User 0
    Autofill will inflate views as the current foreground user instead of
    the system_server user (0).
    
    Bug: 327137311
    Test: atest CtsAutoFillServiceTestCases --user-type secondary_user
    Flag: EXEMPT security bug fix
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:2324e00311c0b3326be4389f2905fed270298864
    Merged-In: I8a2c96259976e61d297dec914d10d86e585a6d6f
    Change-Id: I8a2c96259976e61d297dec914d10d86e585a6d6f

--- services/autofill/java/com/android/server/autofill/Helper.java
+++ services/autofill/java/com/android/server/autofill/Helper.java
@@ -16,10 +16,13 @@
 
 package com.android.server.autofill;
 
+import static android.Manifest.permission.INTERACT_ACROSS_USERS;
+
 import static com.android.server.autofill.Helper.sDebug;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.annotation.RequiresPermission;
 import android.annotation.UserIdInt;
 import android.app.ActivityManager;
 import android.app.assist.AssistStructure;
@@ -32,6 +35,7 @@ import android.content.Context;
 import android.graphics.drawable.Icon;
 import android.hardware.display.DisplayManager;
 import android.metrics.LogMaker;
+import android.os.UserHandle;
 import android.os.UserManager;
 import android.service.autofill.Dataset;
 import android.service.autofill.InternalSanitizer;
@@ -101,6 +105,26 @@ public final class Helper {
         });
 
         return permissionsOk.get();
+    }
+
+    /**
+     * Creates the context as the foreground user
+     *
+     * <p>Returns the current context as the current foreground user
+     */
+    @RequiresPermission(INTERACT_ACROSS_USERS)
+    public static Context getUserContext(Context context) {
+        int userId = ActivityManager.getCurrentUser();
+        Context c = context.createContextAsUser(UserHandle.of(userId), /* flags= */ 0);
+        if (sDebug) {
+            Slog.d(
+                    TAG,
+                    "Current User: "
+                            + userId
+                            + ", context created as: "
+                            + c.getContentResolver().getUserId());
+        }
+        return c;
     }
 
     /**
