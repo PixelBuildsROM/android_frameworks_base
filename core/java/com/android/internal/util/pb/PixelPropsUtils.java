719058e5041d9f2ecc2f268ff9a92812457f034b
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2024-08-24 12:32:49 +0300

    PixelPropsUtils: new Pixel codenames
    * Add more tensor features while at it
    
    Change-Id: I52b4971079458c6649e9c4055af80d438eb13cce

--- core/java/com/android/internal/util/pb/PixelPropsUtils.java
+++ core/java/com/android/internal/util/pb/PixelPropsUtils.java
@@ -90,6 +90,10 @@ public class PixelPropsUtils {
 
     // Codenames for currently supported Pixels by Google
     private static final String[] pixelCodenames = {
+            "comet",
+            "komodo",
+            "caiman",
+            "tokay",
             "akita",
             "husky",
             "shiba",
