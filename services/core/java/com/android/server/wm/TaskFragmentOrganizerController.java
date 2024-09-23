bc7df907d31fa6c5cdbf58e57ea155a6b08aa98e
Louis Chang <louischang@google.com>
2024-09-23 03:47:15 +0000

    Prevent activity token leaked to another process
    Malicious app could register the organizer via one-way binder call
    to disguise as running on pid 0.
    
    Bug: 367266072
    Bug: 446678690
    Test: verified via the sample app
    Flag: EXEMPT bugfix
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:61ab2b65caf855c48fdb4166f94e02bf79c90e7b
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:a0d83002325f7d7ab4216d0533884f461a3c900e
    Merged-In: I51378c7d2da06fb83670abd082a089cfd82d699d
    Change-Id: I51378c7d2da06fb83670abd082a089cfd82d699d

--- services/core/java/com/android/server/wm/TaskFragmentOrganizerController.java
+++ services/core/java/com/android/server/wm/TaskFragmentOrganizerController.java
@@ -347,7 +347,7 @@ public class TaskFragmentOrganizerController extends ITaskFragmentOrganizerContr
             }
 
             final IBinder activityToken;
-            if (activity.getPid() == mOrganizerPid) {
+            if (activity.getPid() == mOrganizerPid && activity.getUid() == mOrganizerUid) {
                 // We only pass the actual token if the activity belongs to the organizer process.
                 activityToken = activity.token;
             } else {
@@ -447,6 +447,11 @@ public class TaskFragmentOrganizerController extends ITaskFragmentOrganizerContr
                 throw new IllegalStateException(
                         "Replacing existing organizer currently unsupported");
             }
+
+            if (pid <= 0) {
+                throw new IllegalStateException("Cannot register from invalid pid: " + pid);
+            }
+
             mTaskFragmentOrganizerState.put(organizer.asBinder(),
                     new TaskFragmentOrganizerState(organizer, pid, uid));
             mPendingTaskFragmentEvents.put(organizer.asBinder(), new ArrayList<>());
