c4a81f185e3400babcf0062f77ee5601b4097663
Sumedh Sen <sumedhsen@google.com>
2024-10-03 18:35:13 +0000

    Check cross user permissions for a given UID
    Instead of relying on Context#checkCallingOrSelfPermission, explicitly
    check permissions against a given UID. However, to maintain legacy
    behavior, replace custom UIDs with Binder.getCallingUid when enforcing
    permissions from a method.
    
    Also update tests afftected by this change - by adding methods to mocked
    objects
    
    Bug: 350456241
    
    Test: sts-tradefed run sts-dynamic-develop -m CtsSecurityTestCases -t android.security.cts.ContentProviderMultiUserTests#testAccessFromInitialUser --user-type PRIMARY
    
    Test: sts-tradefed run sts-dynamic-develop -m CtsSecurityTestCases -t android.security.cts.ContentProviderMultiUserTests --user-type SECONDARY
    
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:6775f07552f15f6e4b934bb9552f7a6abff8060b)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:bb073ca58e32df49cd23d88395a19b46809c2076)
    Merged-In: Ib31cabff5714500471bd397c743e127c85751a5c
    Change-Id: Ib31cabff5714500471bd397c743e127c85751a5c

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -635,11 +635,11 @@ public class ComputerEngine implements Computer {
             String resolvedType, @PackageManager.ResolveInfoFlagsBits long flags, int userId,
             int callingUid, boolean includeInstantApps) {
         if (!mUserManager.exists(userId)) return Collections.emptyList();
-        enforceCrossUserOrProfilePermission(callingUid,
+        enforceCrossUserOrProfilePermission(Binder.getCallingUid(),
                 userId,
                 false /*requireFullPermission*/,
                 false /*checkShell*/,
-                "query intent receivers");
+                "query intent services");
         final String instantAppPkgName = getInstantAppPackageName(callingUid);
         flags = updateFlagsForResolve(flags, userId, callingUid, includeInstantApps,
                 false /* isImplicitImageCaptureIntentAndNotSetByDpc */);
@@ -2156,10 +2156,10 @@ public class ComputerEngine implements Computer {
             return true;
         }
         if (requireFullPermission) {
-            return hasPermission(Manifest.permission.INTERACT_ACROSS_USERS_FULL);
+            return hasPermission(Manifest.permission.INTERACT_ACROSS_USERS_FULL, callingUid);
         }
-        return hasPermission(android.Manifest.permission.INTERACT_ACROSS_USERS_FULL)
-                || hasPermission(Manifest.permission.INTERACT_ACROSS_USERS);
+        return hasPermission(android.Manifest.permission.INTERACT_ACROSS_USERS_FULL, callingUid)
+            || hasPermission(Manifest.permission.INTERACT_ACROSS_USERS, callingUid);
     }
 
     /**
@@ -2172,6 +2172,11 @@ public class ComputerEngine implements Computer {
 
     private boolean hasPermission(String permission) {
         return mContext.checkCallingOrSelfPermission(permission)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean hasPermission(String permission, int uid) {
+        return mContext.checkPermission(permission, /* pid= */ -1, uid)
                 == PackageManager.PERMISSION_GRANTED;
     }
 
@@ -4591,7 +4596,7 @@ public class ComputerEngine implements Computer {
         final boolean listApex = (flags & MATCH_APEX) != 0;
 
         enforceCrossUserPermission(
-                callingUid,
+                Binder.getCallingUid(),
                 userId,
                 false /* requireFullPermission */,
                 false /* checkShell */,
@@ -5143,7 +5148,7 @@ public class ComputerEngine implements Computer {
     @Override
     public int getComponentEnabledSetting(@NonNull ComponentName component, int callingUid,
             @UserIdInt int userId) {
-        enforceCrossUserPermission(callingUid, userId, false /*requireFullPermission*/,
+        enforceCrossUserPermission(Binder.getCallingUid(), userId, false /*requireFullPermission*/,
                 false /*checkShell*/, "getComponentEnabled");
         return getComponentEnabledSettingInternal(component, callingUid, userId);
     }
