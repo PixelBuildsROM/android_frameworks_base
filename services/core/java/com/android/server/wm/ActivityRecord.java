491c70e2ad0a659f707c6a600c0571b40a58f2b4
wilsonshih <wilsonshih@google.com>
2024-12-31 08:25:26 +0000

    Defer remove splash screen while device is locked
    ...and activity does not request showWhenLocked.
    The splash screen won't contains secure information, so it's safe to
    declared as showWhenLocked. But before remove starting window, if the
    activity does not request showWhenLocked and device is locked, try to
    trigger unoccluding animation, and keep app window hide until transition
    animation finish.
    
    Bug: 378088391
    Bug: 383131643
    Test: run simulate app repeatly, verify the app content won't be visible
    during transition animation.
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4d90cac9c80afe090cd231aae8cf4f844d95f8f3)
    Merged-In: Ia2ddece125521eefb15d67e22ea863dfae6af112
    Change-Id: Ia2ddece125521eefb15d67e22ea863dfae6af112

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -225,7 +225,9 @@ import static com.android.server.wm.IdentifierProto.USER_ID;
 import static com.android.server.wm.LetterboxConfiguration.DEFAULT_LETTERBOX_ASPECT_RATIO_FOR_MULTI_WINDOW;
 import static com.android.server.wm.LetterboxConfiguration.MIN_FIXED_ORIENTATION_LETTERBOX_ASPECT_RATIO;
 import static com.android.server.wm.StartingData.AFTER_TRANSACTION_COPY_TO_CLIENT;
+import static com.android.server.wm.StartingData.AFTER_TRANSACTION_IDLE;
 import static com.android.server.wm.StartingData.AFTER_TRANSACTION_REMOVE_DIRECTLY;
+import static com.android.server.wm.StartingData.AFTER_TRANSITION_FINISH;
 import static com.android.server.wm.SurfaceAnimator.ANIMATION_TYPE_APP_TRANSITION;
 import static com.android.server.wm.SurfaceAnimator.ANIMATION_TYPE_PREDICT_BACK;
 import static com.android.server.wm.SurfaceAnimator.ANIMATION_TYPE_RECENTS;
@@ -2814,8 +2816,27 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
         attachStartingSurfaceToAssociatedTask();
     }
 
+    /**
+     * If the device is locked and the app does not request showWhenLocked,
+     * defer removing the starting window until the transition is complete.
+     * This prevents briefly appearing the app context and causing secure concern.
+     */
+    void deferStartingWindowRemovalForKeyguardUnoccluding() {
+        if (mStartingData != null
+                && mStartingData.mRemoveAfterTransaction != AFTER_TRANSITION_FINISH
+                && isKeyguardLocked() && !canShowWhenLockedInner(this) && !isVisibleRequested()
+                && mTransitionController.inTransition(this)) {
+            mStartingData.mRemoveAfterTransaction = AFTER_TRANSITION_FINISH;
+        }
+    }
+
     void removeStartingWindow() {
         boolean prevEligibleForLetterboxEducation = isEligibleForLetterboxEducation();
+
+        if (mStartingData != null
+                && mStartingData.mRemoveAfterTransaction == AFTER_TRANSITION_FINISH) {
+            return;
+        }
 
         if (transferSplashScreenIfNeeded()) {
             return;
@@ -4486,6 +4507,10 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
                 fromActivity.startingMoved = true;
                 tStartingWindow.mToken = this;
                 tStartingWindow.mActivityRecord = this;
+
+                if (mStartingData.mRemoveAfterTransaction == AFTER_TRANSITION_FINISH) {
+                    mStartingData.mRemoveAfterTransaction = AFTER_TRANSACTION_IDLE;
+                }
 
                 ProtoLog.v(WM_DEBUG_ADD_REMOVE,
                         "Removing starting %s from %s", tStartingWindow, fromActivity);
