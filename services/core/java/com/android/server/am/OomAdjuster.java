080019aeebb0d0e8a1ecce12b0bb1fc8456501fa
Andrzej Perczak <linux@andrzejperczak.com>
2025-03-05 10:08:34 +0800

    oom: Enable proactive kills only on modern kernels
    Proactive Kills feature helps with mm especially after long uptimes. I
    observed that zram usage goes up to 100% usage after a few days which
    causes unneded kills and worser multitasking. Proactive kills help
    mitigate this issue, however, this mechanism should be used only on
    modern kernel, ie. with in-kernel LMK disabled, PSI and MGLRU enabled.
    It was observed that on older devices kills are excessive and
    multitasking becomes worse.
    
    To fix this enable PK only on modern kernels, simple path checks should
    be enough to differentiate between supported features.
    
    Change-Id: I8d4c74a42e3b15264b2cc68b4ab455dbc0f867a2
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/am/OomAdjuster.java
+++ services/core/java/com/android/server/am/OomAdjuster.java
@@ -147,6 +147,7 @@ import android.os.IBinder;
 import android.os.PowerManagerInternal;
 import android.os.Process;
 import android.os.RemoteException;
+import android.os.StrictMode;
 import android.os.SystemClock;
 import android.os.Trace;
 import android.util.ArrayMap;
@@ -163,6 +164,7 @@ import com.android.server.am.PlatformCompatCache.CachedCompatChangeId;
 import com.android.server.wm.ActivityServiceConnectionsHolder;
 import com.android.server.wm.WindowProcessController;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -384,6 +386,8 @@ public class OomAdjuster {
     private final ArraySet<ProcessRecord> mPendingProcessSet = new ArraySet<>();
     private final ArraySet<ProcessRecord> mProcessesInCycle = new ArraySet<>();
 
+    private Boolean mProactiveKillsEnabled = null;
+
     /**
      * Flag to mark if there is an ongoing oomAdjUpdate: potentially the oomAdjUpdate
      * could be called recursively because of the indirect calls during the update;
@@ -459,6 +463,25 @@ public class OomAdjuster {
         mTmpQueue = new ArrayDeque<ProcessRecord>(mConstants.CUR_MAX_CACHED_PROCESSES << 1);
         mNumSlots = ((CACHED_APP_MAX_ADJ - CACHED_APP_MIN_ADJ + 1) >> 1)
                 / CACHED_APP_IMPORTANCE_LEVELS;
+    }
+
+    private boolean conditionallyEnableProactiveKills() {
+        if (mProactiveKillsEnabled != null) {
+            return mProactiveKillsEnabled;
+        }
+        boolean isModernKernel = false;
+        StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
+        try {
+            final File mglru = new File("/sys/kernel/mm/lru_gen/enabled");
+            final File psi = new File("/proc/pressure/memory");
+            isModernKernel = mglru.exists() && psi.exists();
+        } catch (Exception e) {
+            Slog.w(TAG, "Error checking kernel features", e);
+        } finally {
+            StrictMode.setThreadPolicy(oldPolicy);
+        }
+        mProactiveKillsEnabled = isModernKernel;
+        return isModernKernel;
     }
 
     void initSettings() {
@@ -1285,7 +1308,7 @@ public class OomAdjuster {
         int numEmpty = 0;
         int numTrimming = 0;
 
-        boolean proactiveKillsEnabled = mConstants.PROACTIVE_KILLS_ENABLED;
+        boolean proactiveKillsEnabled = conditionallyEnableProactiveKills();
         double lowSwapThresholdPercent = mConstants.LOW_SWAP_THRESHOLD_PERCENT;
         double freeSwapPercent =  proactiveKillsEnabled ? getFreeSwapPercent() : 1.00;
         ProcessRecord lruCachedApp = null;
