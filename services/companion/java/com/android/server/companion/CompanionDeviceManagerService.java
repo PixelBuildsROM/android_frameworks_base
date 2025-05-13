861fe92f264e797daf6f74f3bf15c49c6d0d3271
Guojing Yuan <guojing@google.com>
2025-05-13 11:58:34 -0700

    Revoke NLS permission if the last association has been removed
    When last association of a package is removed, the corresponding NLS permission should be removed too.
    
    Bug: 391895151
    Test: manual test + unit test
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:c3c33a70f51717b0663225f2e4490e1384463cca
    Merged-In: I193b0dbb700f891d0dbfcf31cf9548fbd7ca0866
    Change-Id: I193b0dbb700f891d0dbfcf31cf9548fbd7ca0866

--- services/companion/java/com/android/server/companion/CompanionDeviceManagerService.java
+++ services/companion/java/com/android/server/companion/CompanionDeviceManagerService.java
@@ -68,10 +68,12 @@ import android.companion.datatransfer.PermissionSyncRequest;
 import android.companion.utils.FeatureUtils;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManagerInternal;
+import android.content.pm.ResolveInfo;
 import android.content.pm.UserInfo;
 import android.net.MacAddress;
 import android.net.NetworkPolicyManager;
@@ -88,6 +90,7 @@ import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.service.notification.NotificationListenerService;
 import android.util.ArraySet;
 import android.util.ExceptionUtils;
 import android.util.Log;
@@ -438,6 +441,24 @@ public class CompanionDeviceManagerService extends SystemService {
         final String packageName = association.getPackageName();
 
         if (changeType == AssociationStore.CHANGE_TYPE_REMOVED) {
+            // Revoke NLS if the last association has been removed for the package
+            Binder.withCleanCallingIdentity(() -> {
+                if (mAssociationStore.getAssociationsForPackage(userId, packageName).isEmpty()) {
+                    NotificationManager nm = getContext().getSystemService(
+                        NotificationManager.class);
+                    Intent nlsIntent = new Intent(
+                            NotificationListenerService.SERVICE_INTERFACE);
+                    List<ResolveInfo> matchedServiceList = getContext().getPackageManager()
+                            .queryIntentServicesAsUser(nlsIntent, /* flags */ 0, userId);
+                    for (ResolveInfo service : matchedServiceList) {
+                        if (service.getComponentInfo().getComponentName().getPackageName()
+                                .equals(packageName)) {
+                            nm.setNotificationListenerAccessGranted(
+                                    service.getComponentInfo().getComponentName(), false);
+                        }
+                    }
+                }
+            });
             markIdAsPreviouslyUsedForPackage(id, userId, packageName);
         }
 
