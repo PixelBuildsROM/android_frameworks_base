f37aa02494517d27ed14aa8a0d9f984e362ada61
oli <olit@google.com>
2025-05-21 14:38:32 +0000

    Add resolveActivityAsUserForExplicitType api to pm
    allow checking for resolved activity for an intent with a specific type
    
    allowing the intent to declare its own type can lead to a security
    vulnerability if the intent changes its type after the IntentForwarder#canForward
    check.
    
    Use the new API in IntentForwarderActivity to prevent checking the intent's type twice.
    
    Update other call sites of IntentForwarder#canForward to extract the resolved type from the intent and pass in to to the method call
    
    Check the launch package rather than calling package
    
    sanitize intent selector as well as received intent
    
    Bug: 403565650 407763772 397216638
    Flag: EXEMPT bugfix
    Test: manually tested
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:57233409d9bb5aef50373760a9b6551aa618c808)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:4f0b02dea6c4f508c9b3aef8fff9910e80041d30
    Merged-In: I8453f25ed5ddd938718f4d2be4983c881778f281
    Change-Id: I8453f25ed5ddd938718f4d2be4983c881778f281

--- core/tests/coretests/src/com/android/internal/app/IntentForwarderActivityTest.java
+++ core/tests/coretests/src/com/android/internal/app/IntentForwarderActivityTest.java
@@ -285,10 +285,20 @@ public class IntentForwarderActivityTest {
         IntentForwarderWrapperActivity activity = mActivityRule.launchActivity(intent);
 
         ArgumentCaptor<Intent> intentCaptor = ArgumentCaptor.forClass(Intent.class);
-        verify(mIPm).canForwardTo(
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+        verify(mIPm, times(2)).canForwardTo(
                 intentCaptor.capture(), nullable(String.class), anyInt(), anyInt());
         assertEquals(Intent.ACTION_VIEW, intentCaptor.getValue().getAction());
 
+        List<Intent> capturedIntents = intentCaptor.getAllValues();
+        // Verify root intent is checked and sanitized
+        assertEquals(Intent.ACTION_MAIN, capturedIntents.get(0).getAction());
+        assertNull(capturedIntents.get(0));
+        assertNull(capturedIntents.get(0).getPackage());
+        // Verify selector is checked and sanitized
+        assertEquals(Intent.ACTION_VIEW, capturedIntents.get(1).getAction());
+        assertNull(capturedIntents.get(1));
+        assertNull(capturedIntents.get(1).getPackage());
         InstrumentationRegistry.getInstrumentation().waitForIdleSync();
         onView(withId(R.id.icon)).check(matches(isDisplayed()));
         onView(withId(R.id.open_cross_profile)).check(matches(isDisplayed()));
@@ -705,6 +715,12 @@ public class IntentForwarderActivityTest {
         @Override
         public PackageManager getPackageManager() {
             return mPm;
+        }
+
+        @Override
+        public CompletableFuture<ResolveInfo> resolveActivityAsUser(Intent intent,
+                String resolvedType, int flags, int userId) {
+            return resolveActivityAsUser(intent, flags, userId);
         }
 
         @Override
