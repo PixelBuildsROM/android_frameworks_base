57c41ba245304534a3ddfced6dd4c5eae9f9f493
Evan Chen <evanxinchen@google.com>
2025-05-29 17:51:03 +0000

    Limits the display name to 1024 characters
    Bug: 418773283
    Test: cts
    Flag: EXEMPT bugfix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:b73847cdfeb6acbb21b70fc979c9c8cb6038bcfb)
    Merged-In: I29e050c498e813e291af213ce4ddc590559c8b7d
    Change-Id: I29e050c498e813e291af213ce4ddc590559c8b7d

--- core/java/android/companion/AssociationRequest.java
+++ core/java/android/companion/AssociationRequest.java
@@ -233,6 +233,8 @@ public final class AssociationRequest implements Parcelable {
      */
     private boolean mSkipPrompt;
 
+    private static final int DISPLAY_NAME_LENGTH_LIMIT = 1024;
+
     /**
      * Creates a new AssociationRequest.
      *
@@ -419,6 +421,11 @@ public final class AssociationRequest implements Parcelable {
         public Builder setDisplayName(@NonNull CharSequence displayName) {
             checkNotUsed();
             mDisplayName = requireNonNull(displayName);
+            if (displayName.length() > DISPLAY_NAME_LENGTH_LIMIT) {
+                throw new IllegalArgumentException("Length of the display name must be at most "
+                        + DISPLAY_NAME_LENGTH_LIMIT + " characters");
+            }
+
             return this;
         }
 
