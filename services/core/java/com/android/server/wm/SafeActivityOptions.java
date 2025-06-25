40e11995ce93aca8d4f1c5db30403f71808c0b78
Hongwei Wang <hwwang@google.com>
2025-06-25 14:52:39 -0700

    [RESTRICT AUTOMERGE] Disallow PINNED in setLaunchWindowingMode
    - Throw security exception if setLaunchWindowingMode is called with
      WINDOWING_MODE_PINNED
    - Deprecated also test cases that are irrelevant after this change
    
    Flag: EXEMPT security fix
    Bug: 388029380
    Test: atest PinnedStackTests \
                SafeActivityOptionsTest \
                TaskLaunchParamsModifierTests
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:88496615b1c72503b557184c4df2973a757b764d)
    Merged-In: I9d37d41af3d86f785fd5d85503b544552b39cac4
    Change-Id: I9d37d41af3d86f785fd5d85503b544552b39cac4

--- services/core/java/com/android/server/wm/SafeActivityOptions.java
+++ services/core/java/com/android/server/wm/SafeActivityOptions.java
@@ -24,6 +24,7 @@ import static android.Manifest.permission.STATUS_BAR_SERVICE;
 import static android.app.ActivityTaskManager.INVALID_TASK_ID;
 import static android.app.WindowConfiguration.ACTIVITY_TYPE_ASSISTANT;
 import static android.app.WindowConfiguration.ACTIVITY_TYPE_UNDEFINED;
+import static android.app.WindowConfiguration.WINDOWING_MODE_PINNED;
 import static android.app.WindowConfiguration.activityTypeToString;
 import static android.content.pm.PackageManager.PERMISSION_DENIED;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
@@ -385,6 +386,17 @@ public class SafeActivityOptions {
             final String msg = "Permission Denial: starting " + getIntentString(intent)
                     + " from " + callerApp + " (pid=" + callingPid
                     + ", uid=" + callingUid + ") with launchedFromBubble=true";
+            Slog.w(TAG, msg);
+            throw new SecurityException(msg);
+        }
+
+        // setLaunchWindowingMode(PINNED) is not allowed, use ActivityOptions#makeLaunchIntoPip
+        // instead which is a public API.
+        if (options.getLaunchWindowingMode() == WINDOWING_MODE_PINNED) {
+            final String msg = "Permission Denial: starting " + getIntentString(intent)
+                    + " from " + callerApp + " (pid=" + callingPid
+                    + ", uid=" + callingUid + ") with"
+                    + " setLaunchWindowingMode=PINNED";
             Slog.w(TAG, msg);
             throw new SecurityException(msg);
         }
