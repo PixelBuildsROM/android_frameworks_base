3f663c19b643769074197ea8abff783b5b676498
Matías Hernández <matiashe@google.com>
2025-07-03 17:35:17 +0200

    Delete updateNotificationChannelGroupFromPrivilegedListener
    This is an unpublished (AIDL only) method and was never used from NLS (or anywhere else).
    
    Bug: 426205822
    Test: build
    Flag: EXEMPT Code deletion
    (cherry picked from commit 6077349a00fdac577592284eadddd18b73c0262e)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:7b9a08940b6e69c3912a6e3d3e3fc2c13056e889
    Merged-In: I65438b1af637f3ba9b75a85f780bdb7867cc4ba8
    Change-Id: I65438b1af637f3ba9b75a85f780bdb7867cc4ba8

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -5769,17 +5769,6 @@ public class NotificationManagerService extends SystemService {
         }
 
         @Override
-        public void updateNotificationChannelGroupFromPrivilegedListener(
-                INotificationListener token, String pkg, UserHandle user,
-                NotificationChannelGroup group) throws RemoteException {
-            Objects.requireNonNull(user);
-            verifyPrivilegedListener(token, user, false);
-            createNotificationChannelGroup(
-                    pkg, getUidForPackageAndUser(pkg, user), group, false, true);
-            handleSavePolicyFile();
-        }
-
-        @Override
         public void updateNotificationChannelFromPrivilegedListener(INotificationListener token,
                 String pkg, UserHandle user, NotificationChannel channel) throws RemoteException {
             Objects.requireNonNull(channel);
