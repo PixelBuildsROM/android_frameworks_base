2ac3c4d41885be8d4c63029988204479cd2d6781
ExactExampl <xmpl@dontsleep.work>
2025-11-03 18:33:34 +0200

    PackageManager: fix NPE for build-time disabling of components when the package does not exist
    * When we don't ship a package as a system prebuilt, but still want certain components disabled (e.g https://github.com/PixelBuilds-Devices/android_device_google_redbull/commit/c08db5d184476de8c1df11457ad5aed6151b25bb), we end up with an NPE that prevents system from booting
    * As an example, here's what we have in logs when we don't ship GCam and have the above commit applied to a device tree:
    
    10-04 14:11:59.037  3661  3661 V PackageManager: Changing enabled state of com.google.android.GoogleCamera/com.google.android.apps.camera.prewarm.NoOpPrewarmService to false
    10-04 14:11:59.037  3661  3661 I Watchdog: Resuming HandlerChecker: main thread for reason: packagemanagermain. Pause count: 0
    10-04 14:11:59.037  3661  3661 E System  : ******************************************
    10-04 14:11:59.038  3661  3661 E System  : ************ Failure starting system services
    10-04 14:11:59.038  3661  3661 E System  : java.lang.NullPointerException: Attempt to invoke virtual method 'com.android.server.pm.parsing.pkg.AndroidPackageInternal com.android.server.pm.PackageSetting.getPkg()' on a null object reference
    10-04 14:11:59.038  3661  3661 E System  : 	at com.android.server.pm.PackageManagerService.enableComponents(PackageManagerService.java:2462)
    10-04 14:11:59.038  3661  3661 E System  : 	at com.android.server.pm.PackageManagerService.<init>(PackageManagerService.java:2247)
    10-04 14:11:59.038  3661  3661 E System  : 	at com.android.server.pm.PackageManagerService.main(PackageManagerService.java:1622)
    10-04 14:11:59.038  3661  3661 E System  : 	at com.android.server.SystemServer.startBootstrapServices(SystemServer.java:1264)
    10-04 14:11:59.038  3661  3661 E System  : 	at com.android.server.SystemServer.run(SystemServer.java:948)
    10-04 14:11:59.038  3661  3661 E System  : 	at com.android.server.SystemServer.main(SystemServer.java:672)
    10-04 14:11:59.038  3661  3661 E System  : 	at java.lang.reflect.Method.invoke(Native Method)
    10-04 14:11:59.038  3661  3661 E System  : 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:552)
    10-04 14:11:59.038  3661  3661 E System  : 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:949)
    10-04 14:11:59.039  3661  3661 E Zygote  : System zygote died with fatal exception
    
    Change-Id: I29d8e864743812ae46cd3785b843ed8749c5280f

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -2459,12 +2459,18 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
             Slog.v(TAG, "Changing enabled state of " + name + " to " + enable);
             String className = cn.getClassName();
             PackageSetting pkgSetting = mSettings.mPackages.get(cn.getPackageName());
+
+            if (pkgSetting == null) {
+                Slog.v(TAG, "Unable to change enabled state of " + name + " to " + enable +
+                " since package does not exist");
+                continue;
+            }
             AndroidPackage pkg = pkgSetting.getPkg();
-            if (pkgSetting == null || pkg == null
-                    || !AndroidPackageUtils.hasComponentClassName(pkg, className)) {
+            if (pkg == null || !AndroidPackageUtils.hasComponentClassName(pkg, className)) {
                 Slog.w(TAG, "Unable to change enabled state of " + name + " to " + enable);
                 continue;
             }
+
             if (enable) {
                 pkgSetting.enableComponentLPw(className, UserHandle.USER_OWNER);
             } else {
