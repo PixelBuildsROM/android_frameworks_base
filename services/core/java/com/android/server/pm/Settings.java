f33a6a2a0e6c2a1cef43ff355054fd6b76a7abeb
Song Chun Fan <schfan@google.com>
2025-11-11 00:03:48 +0000

    [UidMigration] fix update uninstallation with sharedUserMaxSdkVersion
    When a system app is re-enabled after the update is uninstalled, when
    the system app has shared uid, the current code doesn't support
    directly reusing the disabled package setting. Instead, the preloaded
    version is re-scanned and installed as a new app, which can bring
    breaking behavior of changed UIDs when the manifest has
    sharedUserMaxSdkVersion.
    
    This change fixes the bug where registerExistingAppId fails when it
    comes to shared uid, therefore directly reuses the disabled package
    setting, consistent with the behavior for non-shared-uid system apps.
    
    FLAG: EXEMPT BUGFIX
    Test: manually with system-app-test.sh
    BUG: 454062218
    
    
    (cherry picked from commit 6b5ea2f7fbf50313d46e54e0d8f8c18c398e4869)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:445138b31bc4a08afa70dcae78850d3958dbae97
    Merged-In: I1add6ba4a9c0a30a1c029974e70c21eb7a0fe5d5
    Change-Id: I1add6ba4a9c0a30a1c029974e70c21eb7a0fe5d5

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -922,10 +922,11 @@ public final class Settings implements Watchable, Snappable, ResilientAtomicFile
                 p.getAppId(), p.getVersionCode(), p.getFlags(), p.getPrivateFlags(),
                 p.getUsesSdkLibraries(), p.getUsesSdkLibrariesVersionsMajor(),
                 p.getUsesStaticLibraries(), p.getUsesStaticLibrariesVersions(), p.getMimeGroups(),
-                mDomainVerificationManager.generateNewId());
+                mDomainVerificationManager.generateNewId(), p.hasSharedUser());
         if (ret != null) {
             ret.setAppMetadataFilePath(p.getAppMetadataFilePath());
             ret.getPkgState().setUpdatedSystemApp(false);
+            ret.setSharedUserAppId(p.getSharedUserAppId());
         }
         mDisabledSysPackages.remove(name);
         return ret;
@@ -952,7 +953,7 @@ public final class Settings implements Watchable, Snappable, ResilientAtomicFile
             int pkgFlags, int pkgPrivateFlags, String[] usesSdkLibraries,
             long[] usesSdkLibrariesVersions, String[] usesStaticLibraries,
             long[] usesStaticLibrariesVersions, Map<String, Set<String>> mimeGroups,
-            @NonNull UUID domainSetId) {
+            @NonNull UUID domainSetId, boolean hasSharedUser) {
         PackageSetting p = mPackages.get(name);
         if (p != null) {
             if (p.getAppId() == uid) {
@@ -967,7 +968,7 @@ public final class Settings implements Watchable, Snappable, ResilientAtomicFile
                 pkgPrivateFlags, 0 /*userId*/, usesSdkLibraries, usesSdkLibrariesVersions,
                 usesStaticLibraries, usesStaticLibrariesVersions, mimeGroups, domainSetId);
         p.setAppId(uid);
-        if (mAppIds.registerExistingAppId(uid, p, name)) {
+        if (mAppIds.registerExistingAppId(uid, p, name) || hasSharedUser) {
             mPackages.put(name, p);
             return p;
         }
@@ -3940,7 +3941,7 @@ public final class Settings implements Watchable, Snappable, ResilientAtomicFile
                         cpuAbiOverrideString, appId, versionCode, pkgFlags, pkgPrivateFlags,
                         null /* usesSdkLibraries */, null /* usesSdkLibraryVersions */,
                         null /* usesStaticLibraries */, null /* usesStaticLibraryVersions */,
-                        null /* mimeGroups */, domainSetId);
+                        null /* mimeGroups */, domainSetId, /* hasSharedUser= */ false);
                 if (PackageManagerService.DEBUG_SETTINGS)
                     Log.i(PackageManagerService.TAG, "Reading package " + name + ": appId="
                             + appId + " pkg=" + packageSetting);
