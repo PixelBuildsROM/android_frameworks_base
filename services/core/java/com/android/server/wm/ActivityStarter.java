d3fb9d01cf0e11c3e246c7b47eccb1638ad17765
Louis Chang <louischang@google.com>
2026-01-07 07:52:35 +0000

    Disallow launch-behind animation if BAL blocks
    There's no need to perform the launch-behind animation if the BAL
    blocks.
    
    Bug: 460778875
    Test: locally verified
    Flag: EXEMPT CVE_FIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:34bbada3964e240057515958c510ed26f539d527
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:95f41dbd68ab7991f444b078d3fb8bacf74adfbc
    Merged-In: Iab9bd9fc6ebe7291d2eeec7f697cb877240d1c9e
    Change-Id: Iab9bd9fc6ebe7291d2eeec7f697cb877240d1c9e

--- services/core/java/com/android/server/wm/ActivityStarter.java
+++ services/core/java/com/android/server/wm/ActivityStarter.java
@@ -1300,6 +1300,13 @@ class ActivityStarter {
                 .setSourceRecord(sourceRecord)
                 .build();
 
+        // Reset the launch-behind flag to avoid making it visible if the activity launch should
+        // be blocked.
+        if (r.mLaunchTaskBehind && balCode == BAL_BLOCK) {
+            Slog.w(TAG, "Disallowed launching behind for a background launch");
+            r.mLaunchTaskBehind = false;
+        }
+
         mLastStartActivityRecord = r;
 
         if (r.appTimeTracker == null && sourceRecord != null) {
