64a54c1c368f2954de54e978a83678f70f1c9f42
Matt Casey <mrcasey@google.com>
2026-04-08 14:06:04 +0000

    Fix & speed up IntentForwarderActivityTest
    IntentForwarderActivityTest was waiting for 30 seconds for each test
    because it awaited an executor shutdown that it never requested.  Fixing
    this exposed some race conditions that were being hidden by the delay.
    
    Also:
    - Injecting device provisioning bool instead of writing to global
    settings in the test.
    - Add android.permission.HIDE_OVERLAY_WINDOWS, otherwise
    IntentForwarderActivity crashes when it calls setHideOverlayWindows() in
    the test.
    - Fix some assertions that were backwards (asserting null on
    something that had to be non-null for the other lines to pass).
    - Re-enable the test
    
    Bug: 410925936
    Bug: 397216638
    Bug: 407763772
    Bug: 403565650
    Bug: 461500706
    Test: atest IntentForwarderActivityTest
    Flag: EXEMPT TEST_ONLY
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:7722192034b5aa341b46cf7803bad3050cf6a054
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:17638b33b5121825e2dc747ad11248879b800389
    Merged-In: Iade781e0758c477ca888bd1541e0c53e2dee3761
    Change-Id: Iade781e0758c477ca888bd1541e0c53e2dee3761

--- core/java/com/android/internal/app/IntentForwarderActivity.java
+++ core/java/com/android/internal/app/IntentForwarderActivity.java
@@ -118,7 +118,7 @@ public class IntentForwarderActivity extends Activity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mInjector = createInjector();
-        mExecutorService = Executors.newSingleThreadExecutor();
+        mExecutorService = mInjector.getExecutorService();
 
         Intent intentReceived = getIntent();
         String className = intentReceived.getComponent().getClassName();
@@ -479,8 +479,7 @@ public class IntentForwarderActivity extends Activity  {
     }
 
     private boolean isDeviceProvisioned() {
-        return Settings.Global.getInt(getContentResolver(),
-                Settings.Global.DEVICE_PROVISIONED, /* def= */ 0) != 0;
+        return mInjector.isDeviceProvisioned();
     }
 
     private boolean isTextMessageIntent(Intent intent) {
@@ -638,6 +637,17 @@ public class IntentForwarderActivity extends Activity  {
         public void showToast(String message, int duration) {
             Toast.makeText(IntentForwarderActivity.this, message, duration).show();
         }
+
+        @Override
+        public ExecutorService getExecutorService() {
+            return Executors.newSingleThreadExecutor();
+        }
+
+        @Override
+        public boolean isDeviceProvisioned() {
+            return Settings.Global.getInt(getContentResolver(),
+                    Settings.Global.DEVICE_PROVISIONED, /* def= */ 0) != 0;
+        }
     }
 
     public interface Injector {
@@ -653,5 +663,9 @@ public class IntentForwarderActivity extends Activity  {
         CompletableFuture<ResolveInfo> resolveActivityAsUser(Intent intent, int flags, int userId);
 
         void showToast(String message, int duration);
+
+        ExecutorService getExecutorService();
+
+        boolean isDeviceProvisioned();
     }
 }
